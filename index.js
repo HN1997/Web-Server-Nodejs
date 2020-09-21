4e9731053b9290db4c2af8aa0b6709eb569bac86
Hugo Navillod <hugo.navillod@edu.ece.fr>
2020-09-21 16:52:38 +0200

    Update package.json & index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const http = require('http')
+const fs = require('fs')
+const port = 3000
+
+const server = http.createServer(function(req,res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('Hello Node!');
+  res.end();
+})
+
+server.listen(port, function(error){
+  if(error)
+  {
+    console.log("Error", error)
+  } else {
+    console.log('Server is listening on port ' + port)
+  }
+})
