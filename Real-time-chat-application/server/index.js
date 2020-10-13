0caa18fa6af4b4df2f884c11ac9c824c17c45a1c
Hugo Navillod <hugo.navillod@edu.ece.fr>
2020-10-13 19:19:57 +0200

    updae time chat

--- Real-time-chat-application/server/index.js
+++ Real-time-chat-application/server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const socketio = require('socket.io'); //to show smthing in real time
 const http = require('http');
 
+const {addUser, removeUser, getUser, getUsersInRoom} = require('./users.js');
+
 //later for deployment, our server is going to required a specific port (in process.env.port)
 //or run in port 5000
 const PORT = process.env.PORT || 5000
@@ -13,16 +15,28 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 io.on('connection', (socket) => {
-  console.log('We have a new connection.');
-
   socket.on('join', ({name, room}, callback) => {
-    console.log(name, room);
+      const {error, user} = addUser({id: socket.id, name, room});
 
-    const error = true;
+      if(error) return callback(error);
 
-    if(error) {
-      callback(error : 'error');
-    }
+      socket.emit('message', { user: 'admin', text: `${user.name}, welcome to the room ${user.room}`});
+
+      //send a message to everyone
+      socket.broadcast.to(user.room).emit('message', {user: 'admin', text:`${user.name}, has joined!`});
+
+      //joins a user in a room
+      socket.join(user.room);
+
+      callback();
+  });
+
+  socket.on('SendMessage', (message, callback) => {
+    const user = getUser(socket.id); //specific client user
+
+    io.to(user.room).emit('message', {user: user.name, text: message});
+
+    callback();
   });
 
   socket.on('disconnect', () => {
