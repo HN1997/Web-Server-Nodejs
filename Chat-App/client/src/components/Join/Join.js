fc33276b57f73f350b3be20eb5b3912e73c8ee3c
Hugo Navillod <hugo.navillod@edu.ece.fr>
2020-11-08 17:37:07 +0100

    Chat-app back

---
+++ Chat-App/client/src/components/Join/Join.js
@@ -0,0 +1,29 @@
+//useState is from hooks
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
+
+//importing css
+import './Join.css';
+
+export default function SignIn() {
+    //hooks here : variable name, function setName
+    //passing it an empty string as initial value for our
+    //name state
+    const [name, setName] = useState('');
+    //same for the room
+    const [room, setRoom] = useState('');
+
+    return (
+        <div className="joinOuterContainer">
+            <div className="joinInnerContainer">
+                <h1 className="heading">Join</h1>
+                <div><input placeholder="Name" className="joinInput" type="text" onChange={(event)=>setName(event.target.value)}></input></div>
+                <div><input placeholder="Room" className="joinInput mt-20" type="text" onChange={(event)=>setRoom(event.target.value)}></input></div>
+                <Link onClick={event => (!name || !room) ? event.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
+                    <button className="button mt-20" type="submit">Sign In</button>
+                </Link>
+            </div>
+        </div>
+    )
+}
+
