e937b1efca0b7074be612dbbd127a3d20eaea1da
Hugo Navillod <hugo.navillod@edu.ece.fr>
2020-11-09 18:49:36 +0100

    Modif css

--- ReactLab-04/front-end/src/App.js
+++ ReactLab-04/front-end/src/App.js
@@ -4,8 +4,8 @@ import { jsx } from '@emotion/core'
 
 //Importing components
 import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
 import Main from './components/Main/Main';
+import Footer from './components/Footer/Footer';
 
 
 export default () => {
