a18ecdac4deabb0ce83b1d2fd536e7e8f2885c43
Hugo Navillod <hugo.navillod@edu.ece.fr>
2020-12-01 17:13:52 +0100

    Mise en place du projet : backend/frontend

---
+++ front-end/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+//Target our root element inside the html
+ReactDOM.render(<App/>, document.querySelector('#root'));
