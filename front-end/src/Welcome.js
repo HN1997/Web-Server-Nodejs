cbde98918d0e81ff2588279f8aca322923ec0177
PierreC12033 <pierre.camugli@hotmail.fr>
2020-12-20 21:45:30 +0100

    Welcome text

--- front-end/src/Welcome.js
+++ front-end/src/Welcome.js
@@ -36,6 +36,66 @@ export default () => {
         alignItems="center"
         spacing={5}
       >
+        
+        Welcome to Ece-chat the Web application
+
+        This application was programmed by Hugo Navillod and Pierre Camugli using as a template the code written by David Worms
+
+        Here you can chat ith your friends
+
+        To do so, create a channel using the button on the bottom left of your screen.
+        Then get in that channel.
+        There you can writte messages.
+        But your messages or yet to be seen by anyone.
+        To allow your friends to join you you can use the button at the bottom of your screen.
+        a pop-up will show up, put your friend's email address there.
+        If your friend never went on the web-site send him the link!
+        If he already ent on the app he will now be able to see the channel in his list on the left as you do.
+        You are now able to talk to each other!
+        You can add as many friends as you want in your channels. The sky is the limit!
+        You can even add your friends to a channel you've been invited in !
+        So you can make your friends connect with some of your friends' friends and meet some new people!
+
+        You can also change your avatar and your username !
+        To do so you click on your avatar at the top!
+        There complete the form and don't forget to save your changes!
+
+        If you sent the wrong text to the wrong person you can still save the day because Ece-chat allows you to change and even delete the messages that you sent!
+        Click on the icons near the message you want to handle and here you go!
+        Yet you are not a wizard so you can't modify or delete the messages of your friends!
+
+        Here we follow certain rules so keep them in mind :
+        ⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤
+
+        1. Be respectful, civil, and welcoming. 
+
+        Sometimes discussion can get heated, but you are responsible for your own behavior.
+        This includes not targeting individuals and not posting personal information.
+
+        ⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤
+
+        2. No insults, racism, sexism, homophobia, transphobia, and other kinds of discriminatory speech. 
+
+        We do not welcome these types of speech.
+
+        ⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤
+
+        3. No NSFW content
+
+        This is including chat, avatars, pictures, memes, artworks and nicknames. 
+        -If found to be sending NSFW content, it will result in an instant removal.
+
+        ⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤
+
+        4. Do not misuse or spam in any of the channels 
+
+        Text channels
+        - Do not use several lines to say very little ("line splitting").
+        - Do not say the same message repeatedly.
+        - Do not include unsafe content in your messages, such as shortened links.
+
+
+
         <Grid item xs>
           <div css={styles.card}>
             <ChannelIcon css={styles.icon} />
